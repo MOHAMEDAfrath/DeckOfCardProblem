96e9b8e8672f5c9793429053fb72a0a36fcd565b
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-06 07:50:51 +0530

    [Afrath] Welcome to Deck Of Card Problem

---
+++ DeckOfCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeckOfCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Deck of Cards Problem");
+        }
+    }
+}
